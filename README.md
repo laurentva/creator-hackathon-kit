4e6737a2c59f8937cebaa28fdf4c83e4566be24d
Laurent Van Acker <laurent@pozyx.io>
2019-03-16 10:50:46 +0100

    Added cloud example for JS and API docs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # creator-hackathon-kit
 A collection of scripts and tools for Pozyx Creator hackathon
+
+
+API docs: http://api-docs.cloud.pozyxlabs.com/#/pages/mqtt/tags
--- js/cloud.js
+++ js/cloud.js
@@ -1,1 +1,16 @@
- 
+// Install MQTT library: npm install mqtt
+const mqtt = require('mqtt');
+
+const TOPIC = '5abca60f0e091b0005581409';
+const USERNAME = TOPIC;
+const API_KEY = '030d3ab9-df02-4213-83de-90b31b86920a';
+
+const client = mqtt.connect('wss://mqtt.cloud.pozyxlabs.com:443', {
+  username: USERNAME,
+  password: API_KEY,
+});
+
+client.subscribe(TOPIC);
+client.on('message', (topic, message) => {
+  console.info(message.toString());
+});
